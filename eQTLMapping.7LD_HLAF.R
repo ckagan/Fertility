5085ac67e5990090040d7817cf23dc0509c6212a
ckagan <courtneykagan@gmail.com>
2014-08-29 17:20:50 -0500

    eQTL Mapping using genotyped and imputed
    Cis 200kb eQTL mapping of all SNPs .7 + LD with rs2523393

--- eQTLMapping.7LD_HLAF.R
+++ eQTLMapping.7LD_HLAF.R
@@ -49,3 +49,88 @@ write.table(impdose, 'imputeddose_HLAF.7.txt', quote=F, sep='\t')
 write.table(eqtlgeno, 'QCgeno_HLAF.7.txt', quote=F, sep='\t')
 
 write.table(totalmap, 'QCgeno+impdose_HLAF.7_map.txt', quote=F, sep='\t')
+
+#####eQTL Mapping####
+library("MatrixEQTL")
+useModel = modelLINEAR
+#Example All Genotypes
+SNP_file_name = "QCgeno+impdose_HLAF.7.txt" 
+expression_file_name = "HLARegion_FinalGeneExpr.txt"
+
+#In case of no covariates set the variable covariates_file_name to character() in R ([] in Matlab).
+
+covariates_file_name = character() 
+output_file_name = "eQTL_results_HLAF_imputedandgeno_trans.txt"
+output_file_name.cis = "eQTL_results_HLAF_imputedandgeno_cis.txt"
+
+##Next, choose the p-value threshold. Only associations significant at this level will be saved in the output file. Note that for larger datasets the threshold should be lower. Setting the threshold to a high value for a large dataset may cause excessively large output (many gigabytes).
+
+pvOutputThreshold = 0
+cisDist = 200000
+pvOutputThreshold.cis = 1
+
+##Finally, define the covariance matrix for the error term. This a seldom used parameter. If the matrix is a multiple of identity, set the parameter to numeric() in R ([] in Matlab).
+
+errorCovariance = numeric()
+
+#The next section of the sample code contains three very similar parts loading the files with genotype, gene expression, and covariates. In each part one can set the file delimiter (i.e. tab "\t", comma ",", or space " "), the string indicating missing values, the number of rows of column labels, and the columns of row labels. Finally, one can change the number of the variables in a slice for the file reading procedure (do not change if not sure).
+
+snps = SlicedData$new()
+snps$fileDelimiter = "\t"
+# the TAB character
+snps$fileOmitCharacters = "NA" 
+# denote missing values;
+snps$fileSkipRows = 1 
+# one row of column labels
+snps$fileSkipColumns = 1 
+# one column of row labels
+snps$fileSliceSize = 8000 
+# read file in pieces of 10,000 rows
+snps$LoadFile(SNP_file_name)
+
+gene = SlicedData$new()
+gene$fileDelimiter = '\t'
+# a space
+gene$fileOmitCharacters = 'NA'
+# denote missing values;
+gene$fileSkipRows = 1
+# one row of column labels
+gene$fileSkipColumns = 1
+# one column of row labels
+gene$fileSliceSize = 10000
+# read file in pieces of 10,000 rows
+gene$LoadFile(expression_file_name)
+
+cvrt = SlicedData$new()
+cvrt$fileDelimiter = '\t'
+# the TAB character
+cvrt$fileOmitCharacters = 'NA'
+# denote missing values;
+cvrt$fileSkipRows = 1
+# one row of column labels
+cvrt$fileSkipColumns = 1
+# one column of row labels
+cvrt$fileSliceSize = snps$nCols()+1
+# read file in one piece
+if(length(covariates_file_name)>0) {
+  cvrt$LoadFile(covariates_file_name)
+}
+snpspos = read.table('QCgeno+impdose_HLAF.7_map.txt', header = TRUE, stringsAsFactors = FALSE);
+genepos = read.table('HLARegion_genelocmap.txt', header = TRUE, stringsAsFactors = FALSE);
+
+
+#Finally, the main matrix eQTL function is called:
+me = Matrix_eQTL_main(snps,
+                      gene,
+                      cvrt,
+                      output_file_name,
+                      pvOutputThreshold,
+                      useModel,
+                      errorCovariance,
+                      verbose = TRUE,
+                      output_file_name.cis,
+                      pvOutputThreshold.cis,
+                      snpspos,
+                      genepos,
+                      cisDist,
+                      pvalue.hist = 100)
